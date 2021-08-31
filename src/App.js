59e2472979c685014ad55be92822a8f4fc96783f
ngenohkevin <ngenohkevin19@gmail.com>
2021-08-31 17:24:16 +0300

    THIS IS MY FIRST EVER REACT.JS APP still work in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.css'
+import {
+    Typography,
+    Container,
+    CssBaseline,
+    Card,
+    CardContent,
+    makeStyles,
+    Button,
+    CardActions, Input,
+} from "@material-ui/core";
+import axios from "axios";
+
+
+const useStyles = makeStyles({
+    card: {
+        marginTop: 20,
+        marginBottom: 20,
+        fontWeight: 20
+    },
+    cardContent: {
+        paddingBottom: 10,
+
+    },
+    cardActions:{
+        padding: '40px',
+        WebkitColumnSpan: "inherit"
+    }
+})
 
 function App() {
+
+    const classes = useStyles();
+
+    const [state, setState] = useState({
+        joke: '',
+        cat: '',
+    });
+    const [query, setQuery] = useState('food')
+
+    useEffect(() => {
+        getResults();
+    },[]);
+
+    useEffect( () => {
+        fetchData();
+
+
+    },[]);
+    const randomJoke = () => {
+     // console.log('random joke', state)
+        fetchData();
+    }
+
+    const handleSearch = (e) =>{
+        e.preventDefault()
+        getResults();
+    }
+    const getResults = async () => {
+        return await axios
+            .get(`https://api.chucknorris.io/jokes/search?query=${query}`)
+            .then((res) =>console.log(res.data.result))
+    };
+
+
+    const fetchData = async () => {
+        const result = await axios.get('https://api.chucknorris.io/jokes/random')
+        // console.log(result.data.value);
+
+        setState({
+            ...state,
+            joke: result.data.value,
+            cat: result.data.result
+        });
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CssBaseline/>
+        <Container>
+            <Typography variant = "h1" align="center">
+                Chuck Norris Jokes
+            </Typography>
+            <form onSubmit={handleSearch}>
+                <Input type="text"
+                       onChange={e => setQuery(e.target.value)}
+                       value={query}
+                />
+                <Button type="submit">Search</Button>
+            </form>
+               <Card className={classes.card}>
+                   <CardContent>
+                    <Typography>{state.joke}</Typography>
+
+                   </CardContent>
+               </Card>
+            <CardActions className={classes.cardActions}>
+                <Button variant='contained'
+                        style={{marginTop: 20, marginBottom: 20}}
+                        color='primary'
+                        onClick={() => randomJoke(state.joke)}>
+                    Random joke
+                </Button>
+            </CardActions>
+        </Container>
     </div>
   );
 }
